4cddbc965f5d4dcd373774166f3be3a5a734d4ac
VadymBevz <vbevzv@gmail.com>
2023-03-13 15:55:45 +0200

    fixed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,44 +1,51 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 // Change code below this line
 const galleryContainer = document.querySelector('.gallery');
-function createGalleryCardsMarkup(items) {
-    return items.map(({ preview, original, description }) => {
-        return `
-    <div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-        <img
-            class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}" />
-        </a>
-    </div>
-    `;
-    }).join('');
-};
+const gallery = document.querySelector('.gallery');
 const cardsMarkup = createGalleryCardsMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
-const gallery = document.querySelector('.gallery');
-gallery.addEventListener('click', onGalleryItemClick);
+new SimpleLightbox(".gallery a",{
+  captionDate: "alt",
+  captionDelay: 250,
+})
+function createGalleryCardsMarkup(items) {
+  return items.map(({ preview, original, description }) => {
+      return `
+  <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}" />
+      </a>
+  </div>
+  `;
+  }).join('');
+};
 
-function onGalleryItemClick(event) {
+
+
+// function onGalleryItemClick(event) {
   
-  event.preventDefault();
+//   event.preventDefault();
 
-  if (event.target.classList.contains('gallery__image')) {
+//   if (event.target.classList.contains('gallery__image')) {
     
-    const imageUrl = event.target.dataset.source;
+//     const imageUrl = event.target.dataset.source;
     
-    const instance = basicLightbox.create(`
-      <img src="${imageUrl}" width="800" height="600">
-    `);
+//     const instance = basicLightbox.create(`
+//       <img src="${imageUrl}" width="800" height="600">
+//     `);
     
-    instance.show();
-  }
-}
+//     instance.show();
+//   }
+// }
 console.log(galleryItems);
 
